854a2b79cb4041d3e73ae24542c87d2c61e71d14
Charles Clinton Pustejovsky III <charles@cpustejovsky.com>
2024-08-27 09:02:44 -0400

    feat: add linked list code

--- clrs/datastructures/linkedlist/linkedlist.go
+++ clrs/datastructures/linkedlist/linkedlist.go
@@ -1,6 +1,39 @@
 package linkedlist
 
-type ListNode struct {
-	Val  int
-	Next *ListNode
+type Node struct {
+	Value int
+	Next  *Node
+}
+
+func New(val int) *Node {
+	return &Node{
+		Value: val,
+	}
+}
+
+func Search(n *Node, value int) *Node {
+	if n == nil || value == n.Value {
+		return n
+	}
+	return Search(n.Next, value)
+}
+
+func Walk(n **Node, arr *[]int) {
+	cur := n
+	for ; (*cur) != nil; cur = &(*cur).Next {
+		*arr = append(*arr, (*cur).Value)
+	}
+	// if x != nil {
+	// 	Walk(x.Next, arr)
+	// 	*arr = append(*arr, x.Value)
+	// }
+}
+
+func Insert(n **Node, val int) {
+	cur := n
+	for ; (*cur) != nil; cur = &(*cur).Next {
+	}
+	*cur = &Node{
+		Value: val,
+	}
 }
--- clrs/datastructures/linkedlist/linkedlist_test.go
+++ clrs/datastructures/linkedlist/linkedlist_test.go
@@ -1,1 +1,30 @@
-package linkedlist
+package linkedlist_test
+
+import (
+	"testing"
+
+	"github.com/cpustejovsky/algorithms/clrs/datastructures/linkedlist"
+)
+
+func TestNew(t *testing.T) {
+	want := 42
+	got := linkedlist.New(42)
+	if want != got.Value {
+		t.Fatalf("got %v, wanted %v\n", got.Value, want)
+	}
+}
+
+func TestInsert(t *testing.T) {
+	ll := linkedlist.New(42)
+	var arr []int
+	linkedlist.Walk(&ll, &arr)
+	if len(arr) != 1 {
+		t.Fatalf("got %v, wanted %v\n", len(arr), 1)
+	}
+	linkedlist.Insert(&ll, 43)
+	arr = []int{}
+	linkedlist.Walk(&ll, &arr)
+	if len(arr) != 2 {
+		t.Fatalf("got %v, wanted %v\n", len(arr), 2)
+	}
+}
